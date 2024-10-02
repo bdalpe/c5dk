fc4e173d0bb2235c5feb067216769b26f86271a7
Brendan Dalpe <bdalpe@gmail.com>
2024-10-02 10:08:35 -0500

    Update tests

--- lib/__tests__/certificates.test.ts
+++ lib/__tests__/certificates.test.ts
@@ -1,7 +1,7 @@
 import {it, expect, vi, beforeEach, describe} from 'vitest';
 import {vol} from 'memfs'
 import {Cribl} from "../index";
-import {Certificate} from "../objects/certificates";
+import {Certificate} from "../objects";
 
 vi.mock('node:fs');
 vi.mock('node:fs/promises');
@@ -30,7 +30,6 @@ test
 		cribl.synth();
 
 		const volume = vol.toJSON();
-		console.log(volume);
 		expect(Object.keys(volume)).toEqual(expect.arrayContaining(['/tmp/local/cribl/certificates.yml', '/tmp/local/cribl/auth/certs/goat.pem', '/tmp/local/cribl/auth/certs/goat.key']));
 		expect(volume['/tmp/local/cribl/certificates.yml']).toMatchSnapshot();
 		expect(volume['/tmp/local/cribl/auth/certs/goat.pem']).toMatchSnapshot();
